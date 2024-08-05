929501ddc78bc68ebc81bf5d487e91f6cf0762f2
reidn3r <reidnersteam@hotmail.com>
2024-08-05 09:17:14 -0300

    mm: comments

--- matrix/main.py
+++ matrix/main.py
@@ -7,22 +7,22 @@ def main(sum:MatrixSum, multiply:MatrixMultiply, n_threads:int, matrix_a, matrix
     p_start = time.time()
     sum.sum_matrix_parallel(n_threads, matrix_a, matrix_b, output)
     p_end = time.time()
-    print(f'parallel sum: {p_end - p_start} ms')
+    print(f'parallel sum: {p_end - p_start} s')
 
     seq_start = time.time()
     sum.sum_matrix_sequential(matrix_a, matrix_b, output)
     seq_end = time.time()
-    print(f'sequential sum: {seq_end - seq_start} ms\n')
+    print(f'sequential sum: {seq_end - seq_start} s\n')
 
     p_start = time.time()
     multiply.multiply_matrix_parallel(n_threads, matrix_a, matrix_b, output)
     p_end = time.time()
-    print(f'parallel multiply: {p_end - p_start} ms')
+    print(f'parallel multiply: {p_end - p_start} s')
     
     seq_start = time.time()
     multiply.multiply_matrix_sequential(matrix_a, matrix_b, output)
     seq_end = time.time()
-    print(f'sequential multiply: {seq_end - seq_start} ms\n')
+    print(f'sequential multiply: {seq_end - seq_start} s\n')
 
 if __name__ == "__main__":
     np.random.seed(42)
