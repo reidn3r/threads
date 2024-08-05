a4b62e06e787612c44edfca8b219b9deb510a027
reidn3r <reidnersteam@hotmail.com>
2024-08-05 08:27:42 -0300

    mm: multiply matrix block approach

--- matrix/main.py
+++ matrix/main.py
@@ -4,37 +4,31 @@ import numpy as np
 import time
 
 def main(sum:MatrixSum, multiply:MatrixMultiply, n_threads:int, matrix_a, matrix_b, output):
-    p_start = time.time()
-    sum.sum_matrix_parallel(n_threads, matrix_a, matrix_b, output)
-    p_end = time.time()
-    print(f'parallel sum: {p_end - p_start} ms')
+    # p_start = time.time()
+    # sum.sum_matrix_parallel(n_threads, matrix_a, matrix_b, output)
+    # p_end = time.time()
+    # print(f'parallel sum: {p_end - p_start} ms')
 
-    seq_start = time.time()
-    sum.sum_matrix_sequential(matrix_a, matrix_b, output)
-    seq_end = time.time()
-    print(f'sequential sum: {seq_end - seq_start} ms\n')
+    # seq_start = time.time()
+    # sum.sum_matrix_sequential(matrix_a, matrix_b, output)
+    # seq_end = time.time()
+    # print(f'sequential sum: {seq_end - seq_start} ms\n')
 
     p_start = time.time()
     multiply.multiply_matrix_parallel(n_threads, matrix_a, matrix_b, output)
     p_end = time.time()
     print(f'parallel multiply: {p_end - p_start} ms')
     
-    p_start = time.time()
-    multiply.multiply_matrix_sequential(matrix_a, matrix_b, output)
-    p_end = time.time()
-    print(f'parallel multiply: {p_end - p_start} ms')
-
-
-    print(matrix_a, "\n")
-    print(matrix_b, "\n")
-    print(output, "\n")
-
+    # seq_start = time.time()
+    # multiply.multiply_matrix_sequential(matrix_a, matrix_b, output)
+    # seq_end = time.time()
+    # print(f'sequential multiply: {seq_end - seq_start} ms')
 
 if __name__ == "__main__":
     np.random.seed(42)
     sum, mm = MatrixSum(), MatrixMultiply()
 
-    n_threads:int = int(input("soma - n threads: "))
+    n_threads:int = int(input("n threads: "))
     print("matriz: 100x100")
     matrix_a, matrix_b, output = sum.initialize_matrix(100)
     main(sum, mm, n_threads, matrix_a, matrix_b, output)
--- matrix/src/multiply.py
+++ matrix/src/multiply.py
@@ -20,18 +20,47 @@ class MatrixMultiply(Utils):
                     output[i][j] += matrix_a[i][k] * matrix_b[k][j]
 
 
-    def multiply_matrix_parallel(self, n_threads:int, matrix_a, matrix_b, output):
-        n_cols:int = matrix_a.shape[1]
-        column_thread_ratio:int = n_cols // n_threads
-        threads = []
-        for i in range(n_threads):
-            start_column:int = i * column_thread_ratio
-            end_column:int = (i+1) * column_thread_ratio if i != n_threads - 1 else n_cols
+    # def multiply_matrix_parallel(self, n_threads:int, matrix_a, matrix_b, output):
+    #     n_cols:int = matrix_a.shape[1]
+    #     n_threads = min(n_threads, matrix_a.shape[1])
+    #     column_thread_ratio:int = n_cols // n_threads
+    #     threads = []
+        
+    #     for i in range(n_threads):
+    #         start_column:int = i * column_thread_ratio
+    #         end_column:int = (i+1) * column_thread_ratio if i != n_threads - 1 else n_cols
             
-            thread = threading.Thread(target=self.multiply_matrix_sequential, args=(matrix_a, matrix_b, output, start_column, end_column))
-            threads.append(thread)
-            thread.start()
+    #         thread = threading.Thread(target=self.multiply_matrix_sequential, args=(matrix_a, matrix_b, output, start_column, end_column))
+    #         threads.append(thread)
+    #         thread.start()
+    
+    #     self.__join__threads__(threads)
+    #     return output
+
+    def multiply_matrix_block(self, matrix_a, matrix_b, output, start_row, end_row, start_column, end_column):
+        for i in range(start_row, end_row):
+            for j in range(start_column, end_column):
+                output[i, j] = 0
+                for k in range(matrix_a.shape[1]):
+                    output[i, j] += matrix_a[i, k] * matrix_b[k, j]
+
+    
+    def multiply_matrix_parallel(self, n_threads: int, matrix_a, matrix_b, output):
+        n_rows, n_cols = matrix_a.shape[0], matrix_b.shape[1]
+        n_threads = min(n_threads, n_rows * n_cols)
+        
+        block_size_row = max(1, n_rows // int(np.sqrt(n_threads)))
+        block_size_col = max(1, n_cols // int(np.sqrt(n_threads)))
+        
+        threads = []
+        for i in range(0, n_rows, block_size_row):
+            for j in range(0, n_cols, block_size_col):
+                end_row = min(i + block_size_row, n_rows)
+                end_column = min(j + block_size_col, n_cols)
+                
+                thread = threading.Thread(target=self.multiply_matrix_block, args=(matrix_a, matrix_b, output, i, end_row, j, end_column))
+                threads.append(thread)
+                thread.start()
     
         self.__join__threads__(threads)
         return output
-
